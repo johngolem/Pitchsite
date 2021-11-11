ab4af4f3372744ca8e4a3e6d8788bb9a70c76550
John Paul <johnpaul@trees.local>
2021-11-11 15:45:39 +0300

    render template included

--- app/main/views.py
+++ app/main/views.py
@@ -5,9 +5,7 @@ from flask_login import login_required
 
 @main.route('/')
 def index ():
-     '''
-    View root page function that returns the index page and its data
-    '''
+    return render_template("index.html")
 
 @main.route('/pitch//new/<int:id>', methods = ['GET','POST'])
 @login_required
