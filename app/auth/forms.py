4c39a514d8bf7bed54a2ba87d009357c1f1b1fe8
John Paul <johnpaul@trees.local>
2021-11-11 18:29:48 +0300

    add new_pitch htmnl file

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,4 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
+from wtforms.fields.simple import TextAreaField
 from wtforms.validators import Required,Email,EqualTo
-from ..models import User
+from ..models import Pitch, User
+
+
+class PitchForm(FlaskForm):
+
+    title = StringField('Pitch title',validators=[Required()])
+    category=StringField('Pitch Category', validators=[Required()])
+    Pitch = TextAreaField('New Pitch', validators=[Required()])
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,11 @@
 from . import main
 from flask import render_template
 from flask_login import login_required
+from ..models import Pitch
+from ..auth import PitchForm
 
+
+# Pitch = pitch.Pitch
 
 @main.route('/')
 def index ():
@@ -10,5 +14,18 @@ def index ():
 @main.route('/pitch//new/<int:id>', methods = ['GET','POST'])
 @login_required
 def new_pitch(id):
+    form = PitchForm()
+#     pitch = get_pitch(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        category = form.pitch.data
+     #    new_pitch = Pitch(movie.id,title,movie.poster,review)
+        new_pitch.save_pitch()
+     #    return redirect(url_for('movie',id = movie.id ))
+
+#     title = f'{movie.title} review'
+#     return render_template('new_pitch.html',title = title, pitch_form=form, pitch=pitch)
+    
 
 
