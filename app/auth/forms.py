3fd78358a9abb250a0d48700207d958a47194576
John Paul <johnpaul@trees.local>
2021-11-11 18:41:02 +0300

    add login forms

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
 from wtforms.fields.simple import TextAreaField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import Pitch, User
 from wtforms import ValidationError
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
 
 class PitchForm(FlaskForm):
 
@@ -26,3 +26,9 @@ def validate_email(self,data_field):
 def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('That username is taken')
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
