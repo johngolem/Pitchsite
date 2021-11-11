78d22ccb368baf9ca65ce07d2d9ce135a4d1fb93
John Paul <johnpaul@trees.local>
2021-11-11 11:34:36 +0300

    registered the blueprint

--- app/models.py
+++ app/models.py
@@ -6,5 +6,15 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
 
+    # __tablename__ = 'users'
+    # id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # username = db.Column(db.String(20), unique=True,nullable=False)
+    # email = db.Column(db.String(30), unique=True,nullable=False)
+    # password = db.Column(db.String(80))
+    # pitch = db.relationship('Pitch')
+    # liked = db.relationship('PostLike', foreign_keys='PostLike.users_id', backref='users',lazy='dynamic')
+    
+
     def __repr__(self):
         return f'User {self.username}'
+
