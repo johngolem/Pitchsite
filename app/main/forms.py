fc5869301d945ff71843fca181f117be53454901
John Paul <johnpaul@trees.local>
2021-11-17 19:13:54 +0300

    add content to new_pitch.html

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,6 +1,6 @@
 
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField,ValidationError
+from wtforms import StringField,TextAreaField,SubmitField,ValidationError,SelectField
 from wtforms.validators import Required,Email
 
 
@@ -14,6 +14,6 @@ class UpdateProfile(FlaskForm):
 class PitchForm(FlaskForm):
 
     title = StringField('Pitch title',validators=[Required()])
-    category=StringField('Pitch Category', validators=[Required()])
-    Pitch = TextAreaField('New Pitch', validators=[Required()])
+    category = SelectField('Category', choices=[('pickup-lines','pickup'),('interview','interview'),('product','product'),('promotion','promotion')],validators=[Required()])
+    post = TextAreaField('Your Pitch', validators=[Required()])
     submit = SubmitField('Submit')
